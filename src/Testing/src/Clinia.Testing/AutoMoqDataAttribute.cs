c9db6f95d32f9f2c6fc1f4b2867dde1a4bd107e0
Etienne Soulard-Geoffrion <etienne.soulard-geoffrion@clinia.com>
2021-04-19 16:52:37 -0400

    feat(testing): add base testing library

---
+++ src/Testing/src/Clinia.Testing/AutoMoqDataAttribute.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Linq;
+using AutoFixture;
+using AutoFixture.AutoMoq;
+using AutoFixture.Xunit2;
+
+namespace Clinia.Testing
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public class AutoMoqDataAttribute : AutoDataAttribute
+    {
+        public AutoMoqDataAttribute() : base(() => CustomizeFixture(new Fixture()))
+        {}
+
+        public AutoMoqDataAttribute(Func<IFixture> fixtureFactory) : base(() =>
+            {
+                var fixture = fixtureFactory.Invoke();
+                return CustomizeFixture(fixture);
+            })
+        {}
+
+        private static IFixture CustomizeFixture(IFixture fixture)
+        {
+            fixture.Customize(new AutoMoqCustomization());
+            fixture.Behaviors.OfType<ThrowingRecursionBehavior>()
+                .ToList()
+                .ForEach(b => fixture.Behaviors.Remove(b));
+            fixture.Behaviors.Add(new OmitOnRecursionBehavior());
+
+            return fixture;
+        }
+    }
+}
